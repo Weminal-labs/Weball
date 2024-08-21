cce5be0801c002edda71d796670ea933dcad9817
Alexandros De Great <126134422+pichtranst123@users.noreply.github.com>
2024-08-21 21:37:49 +0700

    build: tnx hash when ready room

--- src/components/create-room/WaitingRoom.tsx
+++ src/components/create-room/WaitingRoom.tsx
@@ -236,9 +236,9 @@ const WaitingRoom = ({ open, room, closeRoom, isCreator, openGame }: Pros) => {
       return true;
     }
   };
-  const readyHandle = async (): Promise<void> => {
-    
 
+  
+  const readyHandle = async () => {
     const isReadyUpdated = isCreator
       ? toggleReadyStatus(player1, setPlayer1)
       : toggleReadyStatus(player2, setPlayer2);
@@ -247,17 +247,28 @@ const WaitingRoom = ({ open, room, closeRoom, isCreator, openGame }: Pros) => {
     if (isReadyUpdated) {
       await callContract({
         functionName: "ready_by_room_id",
-        functionArgs:[Number(room?.room_id)],
+        functionArgs: [Number(room?.room_id)],
         onSuccess(result) {
-       
+          const alertContent = (
+            <>
+              Ready Transaction:{" "}
+              <a
+                href={`https://explorer.aptoslabs.com/txn/${result.hash}?network=testnet`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {result.hash}
+              </a>
+            </>
+          );
+          setAlert(alertContent, "success");
         },
         onError(error) {
           console.error("Error executing transaction:", error);
-          setAlert("Transaction failed. Please try again.","error");
+          setAlert("Transaction failed. Please try again.", "error");
         },
-      })
+      });
     }
-    
   };
 
   const handleCloseRoom = async () => {
